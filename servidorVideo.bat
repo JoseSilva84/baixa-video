c6cd45947a1c4625c1ab06a21f25504741deeb73
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-11-04 13:42:09 -0300

    mudanca6

--- servidorVideo.bat
+++ servidorVideo.bat
@@ -1,6 +1,7 @@
 @echo off
 cd C:\Users\curso\Downloads\atividades javascript\baixarVideoRapido
 node server.js
+pause
 cd C:\Users\curso\Downloads\atividades javascript\baixarVideoRapido\public
 start index.html
 pause
